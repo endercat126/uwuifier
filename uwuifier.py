71ffcbcf58f4b9dceab9358f077708f420272def
endercat <enderpug18@gmail.com>
2023-09-13 20:18:54 -0400

    0.7-b2

--- uwuifier.py
+++ uwuifier.py
@@ -2,7 +2,7 @@
 
 # uwuifier.py
 
-VERSION = "0.7-b"
+VERSION = "0.7-b2"
 LICENSE = "MIT"
 
 # Copyright (c) 2023 Endercat126
@@ -13,74 +13,79 @@ import random
 import sys
 import argparse
 import argcomplete
+import json
 
+import ender_ansi as ansi
 
 # --- Data ---
-# colours
-class colours:
-    reset = "\033[0m"
-    bold = "\033[1m"
-    dull = "\033[2m"
-    italic = "\033[3m"
-    underline = "\033[4m"
-    reverse = "\033[7m"
-    black = "\033[30m"
-    red = "\033[31m"
-    green = "\033[32m"
-    yellow = "\033[33m"
-    blue = "\033[34m"
-    pink = "\033[35m"
-    cyan = "\033[36m"
-    white = "\033[37m"
-    bg_black = "\033[40m"
-    bg_red = "\033[41m"
-    bg_green = "\033[42m"
-    bg_yellow = "\033[43m"
-    bg_blue = "\033[44m"
-    bg_pink = "\033[45m"
-    bg_cyan = "\033[46m"
-    bg_white = "\033[47m"
+
+# # some kawaii faces
+# kaomoji = [">.<", ":3", "^-^", "^.^", ">w<", "^.~", "~.^", ">.<", "^o^", "^_^", ">.>", "^3^", "^-^'", "^.^'", ">.<'", "^o^'",
+#            "^_^'", ">.>'", "^3^'", "(*^ω^)", "(*≧ω≦)", "(｡♥‿♥｡)", "(*´∀`)", "(°▽°)", "(╯✧▽✧)╯", "(＾◡＾)", "(⁄ ⁄•⁄ω⁄•⁄ ⁄)", "(*≧▽≦)", "(≧◡≦)"]
+
+# # some cute emojis
+# emojis = {
+#     "happy": ['😺', '😸'],
+#     "sad": ['😿', '😢'],
+#     "angry": ['😡', '😾', '👿'],
+#     "love": ['💖', '💕', '💝', '💘'],
+#     "cute": ['🌸', '⭐', '✨', '🍭', '🍪'],
+#     "surprise": ['😮', '🙀'],
+#     "sus": ['🧐', '😳', '💀']
+# }
+
+# # characters that will be changed
+# substitutions = {
+#     "you": "yu",
+#     "ove": "uv",
+#     "w": "ww",
+#     "r": "w",
+#     "l": "w",
+#     "y ": "yy ",
+#     "- ": "~ "
+# }
+
+# # words that are already cute enough!
+# preserve = ["uwu", "meow", "nyaa", "kawaii"]
 
 
-# some kawaii faces
-kaomoji = [">.<", ":3", "^-^", "^.^", ">w<", "^.~", "~.^", ">.<", "^o^", "^_^", ">.>", "^3^", "^-^'", "^.^'", ">.<'", "^o^'",
-           "^_^'", ">.>'", "^3^'", "(*^ω^)", "(*≧ω≦)", "(｡♥‿♥｡)", "(*´∀`)", "(°▽°)", "(╯✧▽✧)╯", "(＾◡＾)", "(⁄ ⁄•⁄ω⁄•⁄ ⁄)", "(*≧▽≦)", "(≧◡≦)"]
+# # --- Parameters --
 
-# some cute emojis
-emojis = {
-    "happy": ['😺', '😸'],
-    "sad": ['😿', '😢'],
-    "angry": ['😡', '😾', '👿'],
-    "love": ['💖', '💕', '💝', '💘'],
-    "cute": ['🌸', '⭐', '✨', '🍭', '🍪'],
-    "surprise": ['😮', '🙀'],
-    "sus": ['🧐', '😳', '💀']
-}
-
-# characters that will be changed
-substitutions = {
-    "you": "yu",
-    "ove": "uv",
-    "w": "ww",
-    "r": "w",
-    "l": "w",
-    "y ": "yy ",
-    "- ": "~ "
-}
-
-# words that are already cute enough!
-preserve = ["uwu", "meow", "nyaa", "kawaii"]
-
-
-# --- Parameters --
-
-enable_substitutions = True
-enable_kaomoji = True
-enable_emojis = False
-keep_case = False
-debug = False
+# enable_substitutions = True
+# enable_kaomoji = True
+# enable_emojis = False
+# keep_case = False
+# debug = False
 
 # --- Functions ---
+
+def load_config():
+    with open("default_config.json", "r") as f:
+        config = json.load(f)
+    
+    global kaomoji
+    kaomoji = config["kaomoji"]
+
+    global emojis
+    emojis = config["emojis"]
+
+    global substitutions
+    substitutions = config["substitutions"]
+
+    global preserve
+    preserve = config["preserve"]
+
+    global enable_substitutions
+    global enable_kaomoji
+    global enable_emojis
+    global keep_case
+    global debug
+
+    enable_substitutions = config["defaults"]["enable_substitutions"]
+    enable_kaomoji = config["defaults"]["enable_kaomoji"]
+    enable_emojis = config["defaults"]["enable_emojis"]
+    keep_case = config["defaults"]["keep_case"]
+    debug = config["defaults"]["debug"]
 
 def underline(text: str, char: str = "-") -> str:
     line = ""
@@ -110,13 +115,13 @@ def to_spongebob(text: str) -> str:
     return result
 
 
-def print_debug(text: str, colour: str = colours.yellow, error: bool = False) -> None:
+def print_debug(text: str, colour: str = ansi.fg.yellow, error: bool = False) -> None:
     if debug:
         if error:
-            print(colours.red + "ERROR!!!\n" +
-                  text.upper() + "\n!!!" + colours.reset)
+            print(ansi.fg.red + "ERROR!!!\n" +
+                  text.upper() + "\n!!!" + ansi.fx.reset)
         else:
-            print(colour + text + colours.reset)
+            print(colour + text + ansi.fx.reset)
 
 
 def replace_but_keep_case(input_text: str, old_sub: str, new_sub: str) -> str:
@@ -168,7 +173,7 @@ def get_kaomoji() -> str:
 def uwuify(text: str) -> str:
     words = text.split(' ')
 
-    print_debug(str(words), colours.green)
+    print_debug(str(words), ansi.fg.green)
 
     new_words = []
 
@@ -185,13 +190,13 @@ def uwuify(text: str) -> str:
                     else:
                         word = word.lower().replace(sub, substitutions[sub])
 
-                    # print_debug(word, colours.blue)
+                    # print_debug(word, ansi.fg.blue)
 
         new_words.append(word)
 
     text = ' '.join(new_words)
 
-    print_debug(text, colours.cyan)
+    print_debug(text, ansi.fg.cyan)
 
     # get a list of sentences
     sentences = get_sentences(text)
@@ -218,47 +223,59 @@ class CustomArgumentParser(argparse.ArgumentParser):
     def format_help(self):
         custom_help = ""
 
-        custom_help += colours.bold + colours.pink + self.description + colours.blue + " v" + VERSION + colours.reset + "\n"
+        custom_help += ansi.fx.bold + ansi.fg.pink + self.description + ansi.fg.blue + " v" + VERSION + ansi.fx.reset + "\n"
         custom_help += underline(self.description + " v" + VERSION) + "\n\n"
 
-        custom_help += colours.cyan + "Options:" + colours.reset + "\n\n"
+        custom_help += ansi.fg.cyan + "Options:" + ansi.fx.reset + "\n\n"
 
         for action in self._actions:
             line = "  "
 
-            line += colours.green
+            line += ansi.fg.green
             
             for opt in action.option_strings:
                 line += opt + " "
             
-            line += " " * (40 - len(str(action.option_strings)))
+            line += " " * (30 - len(str(action.option_strings)))
 
-            line += colours.yellow
-            line += action.help
+            line += ansi.fg.yellow
+            line += action.help.rstrip('//') + " " + ansi.fx.dull + get_kaomoji()
 
-            line += colours.reset
+            if action.help.endswith('//'):
+                line += "\n"
+
+            line += ansi.fx.reset
             line += "\n"
 
             custom_help += line
 
-        custom_help += "\n"
-        custom_help += colours.reset + "Created by Endercat126" + colours.reset + "\n"
-        custom_help += colours.dull + colours.underline + "https://github.com/endercat126/uwuifier" + colours.reset + "\n\n"
+        custom_help += "\n\n"
+        custom_help += ansi.fg.cyan + "Tip: " + ansi.fx.dull + "put {} around a word to prevent it from being changed" + ansi.fx.reset + "\n"
 
-        custom_help += colours.reset + "This software is licensed under the " + LICENSE + " license." + colours.reset + "\n"
+        custom_help += "\n"
+        custom_help += ansi.fx.reset + "Created by Endercat126" + ansi.fx.reset + "\n"
+        custom_help += ansi.fx.dull + ansi.fx.underline + "https://github.com/endercat126/uwuifier" + ansi.fx.reset + "\n\n"
+
+        custom_help += ansi.fx.reset + "This software is licensed under the " + LICENSE + " license." + ansi.fx.reset + "\n"
 
         return custom_help
 
 # --- Main ---
 
 def cli() -> int:
-    parser = CustomArgumentParser(description="UwUifier: make your text cute!")
+    load_config()
 
-    parser.add_argument('-s', '--enable_substitutions', type=bool, default=True, help='Enable substitutions')
-    parser.add_argument('-k', '--enable_kaomoji', type=bool, default=True, help='Enable kaomoji')
-    parser.add_argument('-e', '--enable_emojis', type=bool, default=False, help='Enable emojis')
-    parser.add_argument('-c', '--keep_case', type=bool, default=False, help='Keep case (experimental)')
-    parser.add_argument('-d', '--debug', type=bool, default=False, help='Enable debugging info')
+    parser = CustomArgumentParser(description="UwUifier: make your text cute!", add_help=False)
+
+    parser.add_argument('-h', '--help', help='Show this help message and exit', action='help')
+    parser.add_argument('-s', '--substitutions', help='Toggle substitutions', action='store_true')
+    parser.add_argument('-k', '--kaomoji', help='Toggle kaomoji', action='store_true')
+    parser.add_argument('-e', '--emojis', help='Toggle emojis', action='store_true')
+    parser.add_argument('-c', '--keep_case', help='Keep case (experimental)', action='store_true')
+    parser.add_argument('-d', '--debug', help='Enable debugging info//', action='store_true')
+    
+    parser.add_argument('-i', '--input', help='Input file', type=str)
+    parser.add_argument('-o', '--output', help='Output file', type=str)
 
     args = parser.parse_args()
 
@@ -268,15 +285,29 @@ def cli() -> int:
     global keep_case
     global debug
 
-    enable_substitutions = args.enable_substitutions
-    enable_kaomoji = args.enable_kaomoji
-    enable_emojis = args.enable_emojis
-    keep_case = args.keep_case
-    debug = args.debug
+    enable_substitutions = not enable_substitutions if args.substitutions else enable_substitutions
+    enable_kaomoji = not enable_kaomoji if args.kaomoji else enable_kaomoji
+    enable_emojis = not enable_emojis if args.emojis else enable_emojis
+    keep_case = not keep_case if args.keep_case else keep_case
+    debug = not debug if args.debug else debug
 
-    text = input("Enter a message:\n" + colours.blue)
-    print(colours.reset)
-    print(colours.pink + uwuify(text))
+    # text = input(ansi.fg.green + "Enter a message:\n" + ansi.fg.blue)
+    # print(ansi.fx.reset)
+    # print(ansi.fg.pink + uwuify(text))
+
+    text = ""
+
+    if args.input:
+        with open(args.input, "r") as f:
+            text = f.read()
+    else:
+        text = input(ansi.fg.green + "Enter a message:\n" + ansi.fg.blue)
+
+    if args.output:
+        with open(args.output, "w") as f:
+            f.write(uwuify(text) + "\n")
+    else:
+        print(ansi.fg.pink + uwuify(text))
 
     return 0
 
